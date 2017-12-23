cde4d822f3f553d9a4878d027b2db1c7995090fe
Michelangelo Bucci <michelangelobucci@gmail.com>
2017-12-23 22:08:36 +0100

    Refactor window system

--- tesseract/Tesseract.py
+++ tesseract/Tesseract.py
@@ -2,7 +2,6 @@ import sys
 from legacy.Tesseract import Tesseract
 
 
-
 if __name__ == "__main__":
     game = Tesseract()
     sys.exit(game.run())
--- tesseract/legacy/Tesseract.py
+++ tesseract/legacy/Tesseract.py
@@ -1,5 +1,6 @@
 import sdl2
-import sdl2.ext
+from sdl2.ext import init as init_graphic_system
+from sdl2.ext import Window, subsurface, fill
 
 import legacy.graphics.renderers as GS
 import legacy.graphics.constants as CONST
@@ -7,32 +8,51 @@ import legacy.Piece as PC
 import legacy.Board as BD
 
 
+class WindowSystem():
+
+    def __init__(self, board_size):
+        init_graphic_system()
+        self.window = Window("Tesseract", size=(800, 600))
+        self.background = self.window.get_surface()
+
+        step = min(30,
+                   int((self.window.size[0] - 20) / ((board_size[0] + 2) * 2)),
+                   int((self.window.size[1] - 20) / (board_size[1])))
+
+        self.board_background = subsurface(self.background,
+                                           (10,
+                                            10,
+                                            (board_size[0] + 2) * step,
+                                            (board_size[1]) * step)
+                                           )
+
+        self.board_surface = subsurface(self.board_background,
+                                        (step,
+                                         step,
+                                         board_size[0] * step,
+                                         (board_size[1] - 2) * step)
+                                        )
+
+        self.next_piece_surface = subsurface(self.background,
+                                             (int(self.window.size[0] / 2) +
+                                              10,
+                                              int(self.window.size[1] / 2) +
+                                              10,
+                                              4 * step,
+                                              4 * step)
+                                             )
+
+
 class Tesseract(object):
 
     def __init__(self):
-        sdl2.ext.init()
-        self.window = sdl2.ext.Window("Tesseract", size=(800, 600))
-        self.background = self.window.get_surface()
-        window = self.window
         self.world = sdl2.ext.World()
-
         self.board = BD.Board(self.world)
-        bsize = self.board.get_board_size()
         self.board_updater = BD.BoardUpdater()
+        bsize = self.board.get_board_size()
 
-        step = min(30, int((window.size[0] - 20) / ((bsize[0] + 2) * 2)),
-                   int((window.size[1] - 20) / (bsize[1])))
-
-        self.board_background = sdl2.ext.subsurface(
-            self.background, (10, 10, (bsize[0] + 2) * step,
-                              (bsize[1]) * step))
-
-        self.board_surface = sdl2.ext.subsurface(self.board_background,
-                                                 (step, step, bsize[0] * step,
-                                                  (bsize[1] - 2) * step))
-        self.next_piece_surface = sdl2.ext.subsurface(self.background, (int(
-            window.size[0] / 2) + 10,
-            int(window.size[1] / 2) + 10, 4 * step, 4 * step))
+        self.ws = WindowSystem(bsize)
+        self.window, self.background = self.ws.window, self.ws.background
 
         self.piecefactory = PC.PieceFactory()
         self.virtual_piece_checker = PC.CollisionSystem()
@@ -44,14 +64,15 @@ class Tesseract(object):
         self.world.add_system(self.virtual_piece_checker)
         self.world.add_system(self.board_updater)
 
-        window.show()
+        self.window.show()
 
-        self.boardrenderer = GS.BoardRenderer(self.board_surface, self.board)
+        self.boardrenderer = GS.BoardRenderer(self.ws.board_surface,
+                                              self.board)
 
         self.np_board = BD.Board(self.world, [[0, 0, 0, 0], [0, 0, 0, 0], [
             0, 0, 0, 0], [0, 0, 0, 0]])
 
-        self.next_piece_renderer = GS.BoardRenderer(self.next_piece_surface,
+        self.next_piece_renderer = GS.BoardRenderer(self.ws.next_piece_surface,
                                                     self.np_board, 0, True)
 
         self.boardrenderer.render_board(self.current_piece)
@@ -108,8 +129,8 @@ class Tesseract(object):
         self.world.process()
 
     def render(self):
-        sdl2.ext.fill(self.background, CONST.BLACK)
-        sdl2.ext.fill(self.board_background, CONST.GREY)
+        fill(self.ws.background, CONST.BLACK)
+        fill(self.ws.board_background, CONST.GREY)
 
         self.boardrenderer.render_board(self.current_piece)
         self.next_piece_renderer.render_board(self.next_piece)
