be09dced827f8b300f0c6d0d775ff0ad84e164a3
Michelangelo Bucci <michelangelobucci@gmail.com>
2017-12-27 19:19:48 +0100

    Add running state

--- tesseract/Game.py
+++ tesseract/Game.py
@@ -1,10 +1,18 @@
 class GameState():
-    def __init__(self):
-        pass
+    name: str
+
+    def __init__(self, name: str = "Generic State") -> None:
+        self.name = name
+
+
+class RunningState(GameState):
+    def __init__(self) -> None:
+        super().__init__("RUNNING")
+        print(self.name)
 
 
 class Game():
     state: GameState
 
     def __init__(self) -> None:
-        self.state = GameState()
+        self.state = RunningState()
--- tests/Test_Game_State.py
+++ tests/Test_Game_State.py
@@ -1,8 +1,14 @@
 import unittest
-from tesseract.Game import Game
+from tesseract.Game import Game, RunningState
 
 
 class TestGame(unittest.TestCase):
-    def test_new_game_has_state(self):
+    def test_new_game_has_state_running(self):
         g = Game()
-        self.assertNotEqual(g.state, None)
+        self.assertEqual(g.state.name, "RUNNING")
+
+
+class TestRunningState(unittest.TestCase):
+    def test_running_state_name(self):
+        rs = RunningState()
+        self.assertEqual(rs.name, "RUNNING")
