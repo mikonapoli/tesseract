81721025575ef69981a519d86064f2bc0030cad5
Michelangelo Bucci <michelangelobucci@gmail.com>
2018-06-24 20:06:10 +0100

    refactor text renderer into its own class

--- tesseract.py
+++ tesseract.py
@@ -1,6 +1,6 @@
 import pygame
 
-class WindowSystem():
+class WindowSystem(object):
     @staticmethod
     def initialise():
         """Initialises the windowing system and returns the main surface"""
@@ -15,6 +15,36 @@ class WindowSystem():
         pygame.display.update()
 
         return screen
+    
+    @staticmethod
+    def get_center(surface):
+        x = int(surface.get_width() / 2)
+        y = int(surface.get_height() / 2)
+        return (x, y)
+
+class TextRenderer(object):
+
+    @staticmethod
+    def get_text_surface(text, size):
+        font = pygame.font.SysFont("None", size)
+        text_surface = font.render(text, True, (255, 255, 255))
+        text_surface = text_surface.convert_alpha()
+        return text_surface
+
+    @classmethod
+    def render_message_on_surface(cls, writing_surface, msg, size=64):
+        lines = msg.split('\n')
+        lines_surfaces = [cls.get_text_surface(line, size) for line in lines]
+        text = msg
+        cx, cy = WindowSystem.get_center(writing_surface)
+        y_offset = int(sum((ts.get_height() for ts in lines_surfaces)) * 0.55)
+        y = cy - y_offset
+        for text_surface in lines_surfaces:
+            x = cx - int(text_surface.get_width() * 0.5)
+            writing_surface.blit(text_surface, (x, y))
+            y += int(text_surface.get_height() * 1.1)
+        pygame.display.flip()
+
         
 
 class Tesseract(object):
@@ -28,31 +58,7 @@ class Tesseract(object):
 
         Press [ESC] to exit
         """
-        self.render_message_on_surface(self.screen, title)
-
-    def get_center(self, surface):
-        x = int(surface.get_width() / 2)
-        y = int(surface.get_height() / 2)
-        return (x, y)
-
-    def write_text(self, text, size):
-        font = pygame.font.SysFont("None", size)
-        text_surface = font.render(text, True, (255, 255, 255))
-        text_surface = text_surface.convert_alpha()
-        return text_surface
-
-    def render_message_on_surface(self, writing_surface, msg, size=64):
-        lines = msg.split('\n')
-        lines_surfaces = [self.write_text(line, size) for line in lines]
-        text = msg
-        cx, cy = self.get_center(writing_surface)
-        y_offset = int(sum((ts.get_height() for ts in lines_surfaces)) * 0.55)
-        y = cy - y_offset
-        for text_surface in lines_surfaces:
-            x = cx - int(text_surface.get_width() * 0.5)
-            writing_surface.blit(text_surface, (x, y))
-            y += int(text_surface.get_height() * 1.1)
-        pygame.display.flip()
+        TextRenderer.render_message_on_surface(self.screen, title)
 
     def run(self):
         running = True
