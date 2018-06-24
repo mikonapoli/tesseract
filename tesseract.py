ff09276c506581e7f43ea6b97f22116615d5c976
Michelangelo Bucci <michelangelobucci@gmail.com>
2018-06-24 17:28:53 +0100

    refactor  in a OO way

--- tesseract.py
+++ tesseract.py
@@ -1,28 +1,27 @@
 import pygame
 
 
-def run_game():
-    pygame.init()
+class Game(object):
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("TESSERACT: A Tetris clone")
 
-    pygame.display.set_caption("TESSERACT: A Tetris clone")
+        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        screen.fill((0, 0, 0))
+        pygame.mouse.set_visible(False)
 
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    screen.fill((0, 0, 0))
+        pygame.display.update()
 
-    pygame.mouse.set_visible(False)
-
-    pygame.display.update()
-
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
 
 
 if __name__ == "__main__":
-    run_game()
+    Game().run()
