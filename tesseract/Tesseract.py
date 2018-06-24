ccaf14e8fb260a9437a4dbd05e26b85e4a56fa4b
Michelangelo Bucci <michelangelobucci@gmail.com>
2018-06-24 13:46:23 +0100

    Destroy and start again

--- tesseract/Tesseract.py
+++
@@ -1,6 +0,0 @@
-import sys
-from legacy import Tesseract
-
-if __name__ == "__main__":
-    game = Tesseract.Game()
-    sys.exit(game.run())
--- tests/Test_Unit_Test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-class TestExample(unittest.TestCase):
-    def test_unit_testing_works(self):
-        very_true = True
-        very_false = False
-        self.assertEqual(very_false, False)
-        self.assertNotEqual(very_false, very_true)
