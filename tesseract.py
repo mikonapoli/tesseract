6bbe071ae73dcbbf834c0431538c7d9318298610
Michelangelo Bucci <michelangelobucci@gmail.com>
2018-06-24 18:43:04 +0100

    Add basic text rendering and main title

--- tesseract.py
+++ tesseract.py
@@ -4,6 +4,7 @@ import pygame
 class Game(object):
     def __init__(self):
         self.screen = self._init_window_system()
+        self._setup_main_screen()
 
     def _init_window_system(self):
         """Initialises the windowing system and returns the main surface"""
@@ -13,15 +14,34 @@ class Game(object):
         pygame.mouse.set_visible(False)
 
         screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        #screen = pygame.display.set_mode((640, 480))
         screen.fill((0, 0, 0))
         pygame.display.update()
 
         return screen
 
+    def _setup_main_screen(self):
+        title = """TESSERACT: A Tetris clone\nPress [ESC] to exit"""
+        self.render_text_on_surface(self.screen, title)
+
     def get_center(self, surface):
         x = int(surface.get_width() / 2)
         y = int(surface.get_height() / 2)
         return (x, y)
+
+    def write_text(self, text, size):
+        font = pygame.font.SysFont("None", size)
+        text_surface = font.render(text, True, (255, 255, 255))
+        text_surface = text_surface.convert_alpha()
+        return text_surface
+
+    def render_text_on_surface(self, writing_surface, text, size=48):
+        x, y = self.get_center(writing_surface)
+        text_surface = self.write_text(text, size)
+        x -= int(text_surface.get_width() / 2)
+        y -= int(text_surface.get_height() / 2)
+        writing_surface.blit(text_surface, (x, y))
+        pygame.display.flip()
 
     def run(self):
         running = True
